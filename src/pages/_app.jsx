5d10da61d67a52a7f2e15bd7060f8693927adacd
Michał Prusak <meehoweq@gmail.com>
2023-05-08 07:06:18 +0200

    Design: Enforce black background for dark theme

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -36,7 +36,7 @@ export default function App({ Component, pageProps, router }) {
    <>
    <DefaultSeo {...SEO} />
         <MDXProvider components={components}>
-      <div className="fixed inset-0 flex justify-center sm:px-8">
+      <div className="fixed inset-0 flex justify-center sm:px-8 dark:bg-black">
         <div className="flex w-full max-w-7xl lg:px-8">
           <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
         </div>
